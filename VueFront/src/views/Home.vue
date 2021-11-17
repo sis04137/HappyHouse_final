ebb82d9b5068d5d9ba8a5af59b81935f0f220125
김혜란 <ss-android@naver.com>
2021-11-17 20:24:50 +0900

    Home.vue

--- VueFront/src/views/Home.vue
+++ VueFront/src/views/Home.vue
@@ -20,9 +20,9 @@
       <!-- Slides with custom text -->
       <!-- 이미지 무엇을 넣을까요오 -->
       <!-- <b-carousel-slide img-src="~/assets/gamsil.jpg"> -->
-      <b-carousel-slide>
-        <b-img :src="require(`@/assets/gamsil.jpg`)"></b-img>
-        <!-- <img :src="require(`@/assets/gamsil.jpg`)" /> -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=1048">
+        <!-- <b-img img-src="https://picsum.photos/id/1/200/300"></b-img> -->
+        <!-- <b-img :src="require(`@/assets/gamsil.jpg`)"></b-img> -->
         <h1>Happy House!!</h1>
         <br /><br /><br /><br />
       </b-carousel-slide>
@@ -30,15 +30,15 @@
       <b-carousel-slide
         caption="Happy House"
         text="아파트 상세 정보를 볼 수 있습니다."
-        img-src="https://picsum.photos/1024/480/?image=52"
+        img-src="https://picsum.photos/1024/480/?image=1031"
       ></b-carousel-slide>
+      <!-- img-src="https://picsum.photos/1024/480/?image=52" -->
 
       <!-- Slide with blank fluid image to maintain slide aspect ratio -->
       <!-- 이미지 무엇을 넣을까요오 -->
       <b-carousel-slide
         caption="행복한 당신의 집 찾아가세요."
-        img-blank
-        img-alt="Blank image"
+        img-src="https://picsum.photos/1024/480/?image=1068"
       >
         <p>
           아파트 정보 페이지를 통해 아파트들의 실거래가를 알 수 있으며, <br />
@@ -46,14 +46,20 @@
         </p>
       </b-carousel-slide>
 
-      <b-carousel-slide caption="made by 정구아">
-        <img :src="require('../assets/gp.jpg')" />
-        <b-img :src="require('../assets/gamsil.jpg')"></b-img>
+      <b-carousel-slide
+        caption="made by 정구아"
+        img-src="https://picsum.photos/1024/480/?image=1065"
+      >
+        <!-- <img :src="require('../assets/gp.jpg')" />
+        <b-img :src="require('../assets/gamsil.jpg')"></b-img> -->
         <p>SSAFY 6기 열정 핫식스!! <br /></p>
       </b-carousel-slide>
 
-      <b-carousel-slide caption="made by 김혜란">
-        <b-img :src="require(`@/assets/hp.jpg`)"></b-img>
+      <b-carousel-slide
+        caption="made by 김혜란"
+        img-src="https://picsum.photos/1024/480/?image=1067"
+      >
+        <!-- <b-img :src="require(`@/assets/hp.jpg`)"></b-img> -->
         <p>서울 12반 3조 입니다!! <br /></p>
       </b-carousel-slide>
     </b-carousel>
