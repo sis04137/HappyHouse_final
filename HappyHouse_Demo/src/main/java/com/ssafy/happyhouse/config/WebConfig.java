7038b2b8ae0fbbffade3017207dcfcda9a04b415
정구아 <wjdrndk96@gmail.com>
2021-11-18 19:23:43 +0900

    CORS error

---
+++ HappyHouse_Demo/src/main/java/com/ssafy/happyhouse/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.ssafy.happyhouse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+//			.allowedOrigins("http://localhost:8080", "http://localhost:8081")
+			.allowedMethods("GET", "POST", "PUT", "DELETE")
+			.maxAge(6000);
+	}
+
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+	    registry.addViewController("/").setViewName("forward:/index.html");
+	}
+	
+}
