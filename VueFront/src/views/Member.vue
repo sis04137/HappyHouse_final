caa69c857f57c776430bbb15e4fd6b37b5f12b38
정구아 <wjdrndk96@gmail.com>
2021-11-25 22:33:49 +0900

    member footer 추가

--- VueFront/src/views/Member.vue
+++ VueFront/src/views/Member.vue
@@ -1,10 +1,23 @@
 <template>
-  <router-view></router-view>
+  <div class="bv-example-row mt-3 text-center">
+    <!-- <h3 class="underline-hotpink"> -->
+    <h2>
+      <!-- <v-icon size="30px">{{ question }}</v-icon -->
+      MyPage
+      <v-icon large size="30px" color="#30ac7c">mdi-account-circle</v-icon>
+    </h2>
+    <router-view></router-view>
+    <footer-bar></footer-bar>
+  </div>
 </template>
 
 <script>
+import FooterBar from "@/components/layout/FooterBar.vue";
 export default {
   name: "Member",
+  components: {
+    FooterBar,
+  },
 };
 </script>
 
